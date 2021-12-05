b00963b5df5492f3f77fa926fd613d5f4882c363
shr-NaHCO3 <shrshr2@163.com>
2021-12-05 17:12:33 +0800

    update something news

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ from flask import (
 )
 from gevent import pywsgi
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-        DATABASE=os.path.join('./database', 'db.sqlite')
-    )
+# def create_app():
+app = Flask(__name__)
+app.config.from_mapping(
+    DATABASE=os.path.join('./database', 'db.sqlite')
+)
 
-    from database import database
-    database.init_app(app)
+from database import database
+database.init_app(app)
 
-    @app.route('/')
-    def home():
-        return render_template('index.html')
-    
-    @app.route('/auth/register')
-    def auth_register():
-        return render_template('auth/register.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/auth/register')
+def auth_register():
+    return render_template('auth/register.html')
 
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-    server.serve_forever()
+server = pywsgi.WSGIServer(('0.0.0.0', 12345), app)
+server.serve_forever()
 
-    return app
+# return app

4000435f2fd5461f9aa681b910bea45f534dff3a
shr-NaHCO3 <shrshr2@163.com>
2021-12-12 01:05:12 +0800

    优化注册体验

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # share-your-file
 you can share you file to others.
+
+# get started
+1. mask sure you installed **`flaskr`, `gevent`, and `execjs`**.
+2. implement database with command **`flask init-db`**
+3. **`flask run`!**
+
+
+# user ID
+user ID = int(str(random.randint(10,99))+str(int(time.time()*1000)))
+
+
+# fileNode
+> filesharer/ *`(main forder)`*
+>
+>> database/ *`(implement database)`*
+>>
+>>> database.py *`(implement database commands and functions)`*
+>>>
+>>> db.sql *`(database commands)`*
+>
+>> static/ *`(static files)`*
+>>
+>>> css/
+>>
+>>> fonts/
+>>
+>>> js/
+>>>
+>>>> md5.js *`(md5 encryption)`*
+>
+>> templates/ *`(Jinja templates)`*
+>
+>> app.py
+>
+>> 历史小测.md (雾霾)
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import os
+import os,time,random
+import re
+import flask
 from flask import (
     Flask, render_template, request
 )
 from gevent import pywsgi
 import execjs
+
+
 
 
 
@@ -22,26 +26,58 @@ database.init_app(app)
 
 
 
+
+
 # 路由
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/auth/register')
+@app.route('/auth/register', methods=('GET', 'POST'))
 def auth_register():
-    return render_template('auth/register.html')
-
-@app.route('/auth/login', methods=('GET', 'POST'))
-def auth_login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
         password2 = request.form.get('password2')
+        db = database.get_db()
+        errors = None
+        if not username:
+            errors = "请输入用户名。"
+        if not password or len(password)<=6:
+            errors = "请输入不少于六位的密码。"
+        if password != password2:
+            errors = "前后密码不一致。请重试。"
 
-        return str([username,password,password2])
+        try:
+            db.execute(
+                "INSERT INTO user (id,username,password,class,jointime) VALUES (?,?,?,?,?)",
+                (
+                    int(str(random.randint(10,99))+str(int(time.time()*1000))),
+                    username,
+                    password,
+                    'peo',
+                    str(time.time())
+                )
+            )
+            db.commit()
+        except db.IntegrityError:
+            errors = "用户已存在！请重试！"
 
+        if errors:
+            return render_template('auth/register.html', error=errors)
+        return flask.redirect('/auth/login')
     
+    return render_template('auth/register.html')
+
+@app.route('/auth/login', methods=('GET', 'POST'))
+def auth_login():
     return "awa?"
 
-server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
-server.serve_forever()
+
+
+
+
+
+# server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
+# server.serve_forever()
+if __name__ == "__main__": app.run('0.0.0.0', '5000')

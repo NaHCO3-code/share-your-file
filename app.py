84b51adadb073912e7322e44c091c29e93ab6e58
shr-NaHCO3 <shrshr2@163.com>
2021-12-16 23:11:03 +0800

    add login

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from abc import update_abstractmethods
 import os,time,random
 import re
 import flask
@@ -16,6 +17,7 @@ app = Flask(__name__)
 app.config.from_mapping(
     DATABASE=os.path.join('./database', 'db.sqlite')
 )
+app.secret_key = 'dev'
 
 # 定义数据库
 from database import database
@@ -36,11 +38,14 @@ def home():
 @app.route('/auth/register', methods=('GET', 'POST'))
 def auth_register():
     if request.method == 'POST':
+        # 获取表单
         username = request.form.get('username')
         password = request.form.get('password')
         password2 = request.form.get('password2')
         db = database.get_db()
         errors = None
+
+        # 校验信息
         if not username:
             errors = "请输入用户名。"
         if not password or len(password)<=6:
@@ -48,6 +53,7 @@ def auth_register():
         if password != password2:
             errors = "前后密码不一致。请重试。"
 
+        # 注册用户
         try:
             db.execute(
                 "INSERT INTO user (id,username,password,class,jointime) VALUES (?,?,?,?,?)",
@@ -63,15 +69,47 @@ def auth_register():
         except db.IntegrityError:
             errors = "用户已存在！请重试！"
 
+        # 发送错误信息
         if errors:
             return render_template('auth/register.html', error=errors)
-        return flask.redirect('/auth/login')
-    
+
+        #注册成功并创建session
+        flask.session.clear()
+        flask.session['username'] = username
+        flask.session['password'] = password
+        return '<a href="/" id="a">登陆成功!去首页</a><script>setInterval(()=>{document.getElementById("a").click()},500)</script>'
+
     return render_template('auth/register.html')
 
 @app.route('/auth/login', methods=('GET', 'POST'))
 def auth_login():
-    return "awa?"
+    if request.method == 'POST':
+        # 获取表单
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = database.get_db()
+        errors = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        # 校验信息
+        if not user:
+            errors = '用户不存在！'
+        elif user['password'] != password:
+            errors = '密码错误！'+user['password']+" "+password
+        if errors == None:
+            flask.session.clear()
+            flask.session['username'] = username
+            flask.session['password'] = password
+            return '<a href="/" id="a">登陆成功!去首页</a><script>setInterval(()=>{document.getElementById("a").click()},500)</script>'
+        
+        return render_template('auth/login.html', error=errors)
+
+    return render_template('auth/login.html')
+
+
+
 
 
 
@@ -80,4 +118,4 @@ def auth_login():
 
 # server = pywsgi.WSGIServer(('0.0.0.0', 5000), app)
 # server.serve_forever()
-if __name__ == "__main__": app.run('0.0.0.0', '5000')
+if __name__ == "__main__": app.run(host='0.0.0.0', port='5000')
